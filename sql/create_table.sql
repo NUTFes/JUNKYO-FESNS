f277b49956bf3017c65f144881e1931ad807891d
YushinOgasawara <168957325+YushinOgasawara@users.noreply.github.com>
2024-09-22 23:55:08 +0900

    ER図をもとにdocker起動時に作成されるデータベースをSQLに記述

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,9 +1,13 @@
-drop table if exists users cascade;
--- ----------------------------------------------------------------------------------------------------
+CREATE TABLE dtb_areas (
+    area_id smallint PRIMARY KEY,    -- エリアID
+    area_name text NOT NULL,         --エリア名
+    area_coordinate text NOT NULL    --座標(x1 y1)または(x+a y+b)
+);
 
-create table users (
-    id serial not null,             -- ID
-    name text not null,             -- ユーザー名
-    created_at timestamp not null,  -- 作成日時
-    primary key (id)
+CREATE TABLE dtb_posts (
+    id integer PRIMARY KEY,                                     -- ID
+    content text NOT NULL,                                      -- 投稿テキスト
+    area_id smallint REFERENCES dtb_areas (area_id),            -- エリアID(外部キー)
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,  -- 作成日時
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP   -- 更新はプログラム側で
 );
