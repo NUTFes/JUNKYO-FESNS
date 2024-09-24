297eeed5c63e31a43d395e075a6a4f7ae7b98264
YushinOgasawara <168957325+YushinOgasawara@users.noreply.github.com>
2024-09-24 23:24:52 +0900

    [fix]markerが表示されないバグを修正

--- next-prisma-postgresql/src/app/components/LeafletMap.tsx
+++ next-prisma-postgresql/src/app/components/LeafletMap.tsx
@@ -3,6 +3,9 @@
 import { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 export default function LeafletMap() {
     //useCallback使った方よくね？
@@ -13,6 +16,17 @@ export default function LeafletMap() {
       minZoom: -1,
       maxZoom: 1,
     });
+    const DefaultIcon = L.icon({
+      iconUrl: icon.src,
+      iconRetinaUrl: iconRetina.src,
+      shadowUrl: iconShadow.src,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41]
+    });
+    L.Marker.prototype.options.icon = DefaultIcon;
 
     // 画像のサイズを定義 (幅, 高さ)
     const imageBounds: L.LatLngBoundsExpression = [[0, 0], [1629, 2403]]; // カスタムイメージのサイズに合わせる
