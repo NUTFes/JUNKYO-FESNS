e55261c30855b5194b9e91ba32cead3dff30f99c
oniwawa <s243336@stn.nagaokaut.ac.jp>
2024-09-26 02:07:03 +0900

    テキストボックスなどのレイアウトの調節、ドロップダウンリストにCSSを適応して仮に表示

--- next-prisma-postgresql/src/app/page.tsx
+++ next-prisma-postgresql/src/app/page.tsx
@@ -4,6 +4,7 @@ import SelectButton from '@/components/bottons/SelectBotton';
 import { Box } from "@mui/material";
 import { Suspense } from "react";
 import dynamic from 'next/dynamic';
+import SelectButtonSample from '@/components/bottons/Select-sample';
 
 // LeafletMapはクライアントサイドのみでレンダリングされるように設定
 const LeafletMap = dynamic(() => import('../components/map/LeafletMap'), {
@@ -14,12 +15,13 @@ const LeafletMap = dynamic(() => import('../components/map/LeafletMap'), {
 export default async function Post() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
         <div>
           <LeafletMap />
         </div>
-        <div>
+        <div className="">
           <SelectButton />
+          <SelectButtonSample />
         </div>
         <Box>
           <Suspense fallback={<div>Loading...</div>}>
