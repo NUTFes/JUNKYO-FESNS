e51c99717989d5c90d18e77305af17b229d4932d
uchida189 <perukipoo@icloud.com>
2024-09-26 14:13:16 +0900

    [fix]入力フォームのレイアウト整え

--- next-prisma-postgresql/src/components/posts/PostForm.tsx
+++ next-prisma-postgresql/src/components/posts/PostForm.tsx
@@ -69,13 +69,15 @@ export default function PostForm() {
         <div className="box">
           <div className="post-flex">
             <div className="comment-main">
-              <input
-                id="content"
-                className="rectangle"
-                placeholder=" フェス、たのしんでる？"
-                {...register("content", { required: true })}//inoutタグから情報取得
-              />
-            </div>
+              {/* <div className="content-wrapper"> */}
+                <input
+                  id="content"
+                  className="rectangle"
+                  placeholder=" フェス、たのしんでる？"
+                  {...register("content", { required: true })}//inoutタグから情報取得
+                />
+              </div>
+            {/* </div> */}
             <div>
               <input className="button" type="image" src="/images/icon_full.svg" disabled={!isValid}></input>
             </div>
