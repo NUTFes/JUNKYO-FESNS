51bdc01ad8239c606e453bec09f2f16d9eedbf6c
uchida189 <perukipoo@icloud.com>
2024-09-26 10:04:11 +0900

    [feat]画面全体にマップが表示されるように修正

--- next-prisma-postgresql/src/app/page.tsx
+++ next-prisma-postgresql/src/app/page.tsx
@@ -11,17 +11,28 @@ const LeafletMap = dynamic(() => import('../components/map/LeafletMap'), {
 
 export default async function Post() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center">
-        <div>
-          <LeafletMap />
-        </div>
-        <Box>
-          <Suspense fallback={<div>Loading...</div>}>
-            <PostForm />
-          </Suspense>
-        </Box>
-      </main>
+    <div
+      id="map"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        // overflowY: 'hidden', // 縦スクロールを無効化
+        whiteSpace: 'nowrap' // 横スクロール可能にするため
+      }}
+    >
+      <LeafletMap />
     </div>
+    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    //   <main className="flex flex-col gap-8 row-start-2 items-center">
+    //     <div>
+    //       <LeafletMap />
+    //     </div>
+    //     <Box>
+    //       <Suspense fallback={<div>Loading...</div>}>
+    //         <PostForm />
+    //       </Suspense>
+    //     </Box>
+    //   </main>
+    // </div>
   );
 }
