50337dbe201a8445e867b1a59c1df16d4c9a52ad
uchida189 <perukipoo@icloud.com>
2024-09-26 10:04:11 +0900

    [feat]画面全体にマップが表示されるように修正

--- next-prisma-postgresql/src/components/map/LeafletMap.tsx
+++ next-prisma-postgresql/src/components/map/LeafletMap.tsx
@@ -3,8 +3,6 @@
 import { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import icon from "leaflet/dist/images/marker-icon.png";
-import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
 import { fetcher } from "@/utils/fetcher";
 import useSWR from "swr";
 import { Areas } from '@/constant/Area';
@@ -36,24 +34,25 @@ export default function LeafletMap() {
   
   //useCallback使った方よくね？
   useEffect(() => {
+    let windowHeight = window.innerHeight;  // ウィンドウの高さを取得
+    let imageHeight = 1629;  // 画像の高さを設定
+    let imageWidth = 2403;  // 画像の幅を設定
+    let aspectRatio = imageWidth / imageHeight;  // 画像のアスペクト比を計算
+    
+    console.log(windowHeight);
+    
     // Leafletマップの初期化
     const map = L.map('map', {
       crs: L.CRS.Simple, // カスタムイメージの場合はSimple座標を使用
       minZoom: -1,
-      maxZoom: 1,
+      maxZoom: -1,
+      maxBounds: [[0, 0], [windowHeight * 2, (windowHeight * 2) * aspectRatio]],  // 縦方向のスクロールを制限
+      maxBoundsViscosity: 1.0,  // マップの端に近づくとスクロールが止まるようにする
+      zoomControl: false,  // 必要に応じてズームコントロールを無効にする
     });
-    const DefaultIcon = L.icon({
-      iconUrl: icon.src,
-      iconRetinaUrl: iconRetina.src,
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      tooltipAnchor: [16, -28],
-    });
-    L.Marker.prototype.options.icon = DefaultIcon;
 
     // 画像のサイズを定義 (幅, 高さ)
-    const imageBounds: L.LatLngBoundsExpression = [[0, 0], [1629, 2403]]; // カスタムイメージのサイズに合わせる
+    const imageBounds: L.LatLngBoundsExpression = [[0, 0], [windowHeight * 2, (windowHeight * 2) * aspectRatio ]]; // カスタムイメージのサイズに合わせる
 
     // 画像タイルを背景に設定
     L.imageOverlay('/images/map.png', imageBounds).addTo(map);
@@ -106,7 +105,8 @@ export default function LeafletMap() {
       // マップのクリーンアップ
       map.remove();
     };
-  }, [data, isLoading, isError]);
+  }, [data, isLoading, isError, window.innerHeight]);
 
-  return <div id="map" style={{ height: '500px', width: '1000px' }} />;
+  // return <div id="map" style={{ height: '500px', width: '1000px' }} />;
+  return <div id="map" style={{ height: '100vh'}} />;
 }
